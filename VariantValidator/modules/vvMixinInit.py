d797235225805b862fab22cf3c793c938771116f
NGeneBio <support@ngenebio.com>
2020-07-03 18:58:47 +0900

    Add MySQL port config

--- VariantValidator/modules/vvMixinInit.py
+++ VariantValidator/modules/vvMixinInit.py
@@ -80,6 +80,7 @@ class Mixin:
             'user':     config["mysql"]["user"],
             'password': config["mysql"]["password"],
             'host':     config["mysql"]["host"],
+            'port':     config["mysql"]["port"],
             'database': config["mysql"]["database"],
             'raise_on_warnings': True
         }
